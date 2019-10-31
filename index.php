54f79d54c49e7a08734ef6780b9e2f3a36a2fe29
root <root@woodicmpl01b.woo.sing.seagate.com>
2019-10-31 21:41:11 +0800

    changed index page content.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Hello World!</title>
 </head>
 <body>
-<?php echo "Hello World! s2itest "; ?>
+<?php echo "Hello World! s2itest version2 "; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 <?php
 }
