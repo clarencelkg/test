c01ac170c703d5935068b569b2707f00fc4f8044
root <root@test1.localdomain>
2020-11-23 14:49:04 +0800

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Hello World!</title>
 </head>
 <body>
-<?php echo "Hello World! s2itest version2 "; ?>
+<?php echo "Hello World! change is on it way "; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 <?php
 }
